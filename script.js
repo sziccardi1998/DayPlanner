f7ff0aa44491fb5d365ee94553a8b2017b73a98c
sziccardi1998 <sziccardifullstack@gmail.com>
2020-11-10 17:30:07 -0500

    Variables added to script.js.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-$("#1pInput").attr("placeholder", "This is a test of jQuery");
+// start on webpage load
+$(document).ready(function () {
+
+    // create variables to be used
+    var nineAM = $("#nineAM");
+    var tenAM = $("#tenAM");
+    var elevenAM = $("#elevenAM");
+    var noon = $("#noon");
+    var onePM = $("#onePM");
+    var twoPM = $("#twoPM");
+    var threePM = $("#threePM");
+    var fourPM = $("#fourPM");
+    var fivePM = $("#fivePM");
+
+    // create function to test getting teh text in text area
+
+    console.log(nineAM.children());
+
+    console.log($(".row").children());
+
+    $(".row").each(function (index) {
+
+        var saveLocation = $(this).children(".saveBtn");
+        var areaLocation = $(this).children(".description");
+        $(saveLocation).on("click", function () {
+            var areaValue = areaLocation.contents();
+            console.log(areaValue);
+        })
+
+    });
+
+})
