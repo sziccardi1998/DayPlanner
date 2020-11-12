00300bad6bf3c0244ec63b76ff67d81448602887
sziccardi1998 <sziccardifullstack@gmail.com>
2020-11-12 10:59:56 -0500

    text storage and loading created.

--- script.js
+++ script.js
@@ -13,60 +13,64 @@ $(document).ready(function () {
 
   // create object to hold each hours text
 
-  var savedText = {
-
-  };
+  var savedText = {};
   // create function to test getting the text in text area
 
   $(".row").each(function (index) {
     var currentId = $(this).attr("id");
-    console.log(currentId);
     savedText[currentId] = "";
-    console.log(savedText);
-    checkTime(this);
     $(this)
       .children(".saveBtn")
       .on("click", function () {
         var areaText = $(this).siblings(".description").val();
-        console.log(areaText);
         var rowID = $(this).parent(".row").attr("id");
-        savedText[rowID]= areaText;
-        console.log(savedText);
+        savedText[rowID] = areaText;
         setStoredText();
       });
   });
 
   // create function to handle retrieving of text content from local storage
   function getStoredText() {
-    
+    // retrieve stored text
+    var loadedText = JSON.parse(localStorage.getItem("storedText"));
+    console.log(loadedText);
+    // check to see if retrieved text exsists
+    if (loadedText !== null) {
+      savedText = loadedText;
+      $(".row").each(function () {
+        var rowMatch = $(this).attr("id");
+        var foundText = $(this).children(".description");
+        if (savedText.rowMatch !== "") {
+          foundText.val(savedText[rowMatch]);
+        }
+      });
+    }
   }
 
   // create function to handle storing text content in local storage
   function setStoredText() {
-
+    localStorage.setItem("storedText", JSON.stringify(savedText));
   }
 
+  // load stored text on page load
   getStoredText();
-
 
   // create function to handle time and date related operations
 
   function checkTime() {
     var currentHour = moment().hour();
     // check each of the save button hour values to see if it is past present or future
-    $(".saveBtn").each(function() {
+    $(".saveBtn").each(function () {
       var blockHour = $(this).attr("value");
       var textChange = $(this).siblings(".description");
-      if(currentHour < blockHour){
+      if (currentHour < blockHour) {
         textChange.addClass("future");
-      }
-      else if(currentHour > blockHour){
+      } else if (currentHour > blockHour) {
         textChange.addClass("past");
-      }
-      else if(currentHour == blockHour){
+      } else if (currentHour == blockHour) {
         textChange.addClass("present");
       }
-    })
+    });
   }
 
   checkTime();
